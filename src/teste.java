30342ea45d1b514ce9176a9f4443a14f8ced7e89
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-06-15 16:23:10 -0300

    modificando algumas sintaxes

--- src/teste.java
+++ src/teste.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 public class teste {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] lista1;
+        int[] lista = new int[50];
         int i;
-        lista1 = new int[50];
-        for (i = 0; i < lista1.length; i++) {
-            System.out.println("digite 50 numeros");
-            lista1[i] = scan.nextInt();
+        System.out.println("digite 50 numeros");
+        for (i = 0; i < lista.length; i++) {
+            lista[i] = scan.nextInt();
         }
-    inverso(lista1,i);
-
-
+            inverso(lista);
     }
-    public static int[] inverso(int lista1[], int i){
-        int[] lista_inv = new int[50];
+    public static void inverso(int lista[]){
+        int i;
+        int[] lista_inv = new int[10];
         for (i = 0; i < lista_inv.length; i++) {
-            lista_inv[i] = lista1[49 - i];
-            System.out.println(lista_inv[i]+"   " +lista1[i]);
+            lista_inv[i] = lista[49 - i];
+            System.out.println(lista_inv[i]+"   " +lista[i]);
 
         }
-        return lista_inv;
     }
 
 }
