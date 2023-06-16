f6e04a666943b6c1e521a30d7882b52bd56fa0ae
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-06-15 21:49:57 -0300

    tentativa falha

--- src/maratona.java
+++ src/maratona.java
@@ -1,13 +1,35 @@
 import java.util.Scanner;
 public class maratona {
-        public static void main(String[]args){
+        public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int contador=0,cont;
+                int sorteio= scan.nextInt();
+                int[]numero=new int[sorteio];
+                int[] rep=new int[sorteio];
+                cont= sorteio;
 
+        for (int i=0;i<sorteio;i++){
+                numero[i]= scan.nextInt();
+                rep[0]=numero[i];
+                if (rep[0]==numero[i]){
+                        cont--;
+                        contador++;
+                }else {
+                        cont--;
 
-
-
-
+                }
 
         }
-}
+
+        System.out.println(contador);
+
+
+
+                }
+        }
+
+
+
+
 
 
