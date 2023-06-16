e78a72fcab4910edc89a41494afdf23691983960
Eduardo Rocha dos Santos <du.rocha.santos@gmail.com>
2023-06-16 19:39:15 -0300

    adicionando soma de linhas na matriz

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,15 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int[][] matriz = new int[3][3];
         int i, j;
+        System.out.println("digite um numero");
         for (i = 0; i < matriz.length; i++) {
             for (j = 0; j < matriz.length; j++) {
-                System.out.println("digite um numero");
                 matriz[i][j] = scan.nextInt();
             }
         }
         diagonalp(matriz);
         repeticao(matriz,scan);
+        soma(matriz);
     }
     public static void diagonalp(int[][] matriz) {
 int i,j;
@@ -37,5 +38,20 @@ int i,j;
             }
         }
         System.out.println("esse numero aparece "+repetidor+" vez"+(repetidor>1?"es":""));
+    }
+    public static void soma(int[][] matriz){
+        int i, j=0,contador;
+        int[][]soma=new int[1][1];
+        while (j!=3){
+            contador=j+1;
+            for (i=0;i<3;i++){
+                soma[0][0]+=matriz[i][j];
+            }
+                System.out.println("a soma das linhas na coluna: "+contador+" e "+soma[0][0]);
+            soma[0][0]=0;
+            j++;
+        }
+
+
     }
 }
